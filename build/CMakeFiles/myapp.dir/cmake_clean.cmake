6f8c75fe54b986e82fa91d49718ae8396f1f7a5f
赵程鹏 <zhaochengpeng@zhaochengpengdeMacBook-Pro.local>
2026-09-25 12:48:38 +0800

    重投影计算

---
+++ build/CMakeFiles/myapp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myapp.dir/src/calculation.cpp.o"
+  "CMakeFiles/myapp.dir/src/calculation.cpp.o.d"
+  "myapp"
+  "myapp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myapp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
