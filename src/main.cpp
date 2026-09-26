4ba731d45e6e69e27d8594844defa01f5d1fb5b2
赵程鹏 <zhaochengpeng@zhaochengpengdeMacBook-Pro.local>
2026-09-26 14:31:55 +0800

    清除错误输出

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+#include "calculation.h"
+using namespace std;
+int main()
+{
+    cal();
+    cout<<"请输入实际观测值："<<endl;
+    double a,b;
+    cin>>a>>b;
+    cout<<"残差大小为："<<cal_dis(a,b)<<endl;
+    return 0;
+}
