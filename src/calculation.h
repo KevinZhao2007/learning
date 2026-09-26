1610a513c42baa1a4a67e8eb6a1a4abcad1c98fe
KevinZhao2007 <15047397216@163.com>
2026-09-26 17:27:31 +0800

    Change return type of cal function from double to bool

--- src/calculation.h
+++ src/calculation.h
@@ -3,5 +3,5 @@ extern double x,y,z,tx,ty,tz,rx,ry,rz;//外参
 extern double fx,fy,cx,cy;//内参
 extern double u,v;//像素坐标与欧式距离
 extern double R1[3][3];
-double cal();
+bool cal();
 double cal_dis(double a,double b);
