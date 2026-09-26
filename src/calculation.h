b1402042ffde02673968889f3901687d4c041e3a
KevinZhao2007 <15047397216@163.com>
2026-09-26 14:54:40 +0800

    Update calculation.h to remove 'dis' variable
    Removed 'dis' from external pixel coordinates.

--- src/calculation.h
+++ src/calculation.h
@@ -1,7 +1,7 @@
 #pragma once
 extern double x,y,z,tx,ty,tz,rx,ry,rz;//外参
 extern double fx,fy,cx,cy;//内参
-extern double u,v,dis;//像素坐标与欧式距离
+extern double u,v;//像素坐标与欧式距离
 extern double R1[3][3];
 double cal();
 double cal_dis(double a,double b);
